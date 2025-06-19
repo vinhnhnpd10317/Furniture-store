627f367002d67d5c8345873d3d63712ede19a8dc
vinh <vinhnhnpd10317@gmail.com>
2025-06-19 21:39:03 +0700

    [feat] sua page

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -56,7 +56,7 @@ function App() {
     <>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Clearance Sale - Premi Seven</title>
+      <title>Clearance Sale - Premi Nine Seven</title>
       <link rel="stylesheet" href="style/home.css" />
       <div className="banner">
         <img
